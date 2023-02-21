c6ee9acc52ebd05dd0e095da6d864576baeb99b1
NicMilli <nicbass2@gmail.com>
2023-02-21 11:08:28 -0800

    Final commit - items crossed out when clicked, bold when hovered

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+const {useState} = React;
+
+const GroceryListItem = (props) => {
+  const [isBought, setIsBought] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const style = {
+    textDecoration: isBought ? 'line-through' : 'none',
+    fontWeight: isHovered ? 'bold' : 400,
+    cursor: 'pointer'
+  };
+
+  const onGroceryItemClick = (event) => {
+    setIsBought(true);
+  };
+
+  const onOverHandler = (event) => {
+    setIsHovered(true);
+  };
+
+  const onLeaveHandler = (event) => {
+    setIsHovered(false);
+  };
+
+  return (
+    <li onClick={onGroceryItemClick} style={style} onMouseEnter={onOverHandler} onMouseLeave={onLeaveHandler}>
+      {props.groceryItem}
+    </li>
+  );
+}
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceries.map((groceryItem) => {
+      return <GroceryListItem groceryItem={groceryItem} />
+    })}
+  </ul>
+)
+
+const App = () => (
+  <div>
+    <h2>My Todo List</h2>
+
+    <GroceryList groceries={['Cucumbers', 'Kale']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
